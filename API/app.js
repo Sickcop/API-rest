531dc3ea2d72019a0794923ea67229405eebe6cd
Sickcop <juan,hurtado2@ucp.edu.co>
2024-07-13 12:12:40 -0500

    first commit

---
+++ API/app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT ?? 3000
+app.disable('x-powered-by')
+
+app.get('/', (req, res) => res.json({message: 'Hello World!'}))
+
+app.listen(port, () => console.log(`Example app listening on port http://localhost:${port}`))
